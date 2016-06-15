ab1e624b2d02de21010f4bf7270aaea3626071fc
thln <tamhenrylenguyen@gmail.com>
2016-06-15 12:59:37 -0700

    Homework One Creating and showing the decision tree

---
+++ homeworkOne.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+
+import os
+import subprocess
+
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+def encode_target(df, target_column):
+    """Add column to df with integers for the target.
+
+    Args
+    ----
+    df -- pandas DataFrame.
+    target_column -- column to map to int, producing
+                     new Target column.
+
+    Returns
+    -------
+    df_mod -- modified DataFrame.
+    targets -- list of target names.
+    """
+    df_mod = df.copy()
+    targets = df_mod[target_column].unique()
+    map_to_int = {name: n for n, name in enumerate(targets)}
+    df_mod["Target"] = df_mod[target_column].replace(map_to_int)
+
+    return (df_mod, targets)
+
+def visualize_tree(tree, feature_names):
+    """Create tree png using graphviz.
+
+    Args
+    ----
+    tree -- scikit-learn DecsisionTree.
+    feature_names -- list of feature names.
+    """
+    with open("dt.dot", 'w') as f:
+        export_graphviz(tree, out_file=f,
+                        feature_names=feature_names)
+
+    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
+    try:
+        subprocess.check_call(command)
+    except:
+        exit("Could not run dot, ie graphviz, to "
+             "produce visualization")
+
+#df = pd.read_csv("iris.csv", index_col=0)
+df = pd.read_csv('StudentTrainingData.csv', sep=',')
+print("* df.head()", df.head(), sep="\n", end="\n\n")
+print("* df.tail()", df.tail(), sep="\n", end="\n\n")
+
+print("* day Alcohol:", df["Dalc"].unique(), sep="\n")
+#* iris types: ['1' '2' '3' '4' '5']
+
+df2, targets = encode_target(df, "Dalc")
+print("* df2.head()", df2[["Target", "Dalc"]].head(),
+      sep="\n", end="\n\n")
+print("* df2.tail()", df2[["Target", "Dalc"]].tail(),
+      sep="\n", end="\n\n")
+print("* targets", targets, sep="\n", end="\n\n")
+
+features = list(df2.columns[13:15])
+print("* features:", features, sep="\n")
+
+y = df2["Target"]
+X = df2[features]
+dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
+dt.fit(X, y)
+visualize_tree(dt, features)
